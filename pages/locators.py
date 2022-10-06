bb840903f0133afdf984c7690064115d8b186d4a
Anna Kolomak <anna.kolomak@syncretis.com>
2022-10-06 12:00:24 +0700

    two new tests for product page

--- pages/locators.py
+++ pages/locators.py
@@ -7,4 +7,13 @@ class LoginPageLocators():
     LOGIN_LINK = (By.ID, "login_link")
     LOGIN_FORM = (By.ID, "login_form")
     REGISTRATION_FORM = (By.ID, "register_form")
+
+class ProductPageLocators():
+    ADD_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    PRODUCT_NAME = (By.CSS_SELECTOR,"h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR,"#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color")
+
+    PRODUCT_IN_BASKET_NAME = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    PRODUCT_IN_BASKET_PRICE = (By.CSS_SELECTOR,"#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
     
+    SUCCESS_MESSAGE  = (By.CSS_SELECTOR, "#messages > div:nth-child(1)")
